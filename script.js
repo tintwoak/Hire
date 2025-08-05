4c907e06c37221f9fb80047b9554e92b2f8201ae
tintwoak <tintwoak@gmail.com>
2025-08-05 15:05:15 +0100

    Update script.js
    Button Removed

--- script.js
+++ script.js
@@ -32,17 +32,6 @@ if (password === "cochin4422") {
   updateCountdown();
   setInterval(updateCountdown, 1000);
 
-// Simple JavaScript to handle button click
-const btn = document.getElementById('notifyBtn');
-const message = document.getElementById('message');
-
-btn.addEventListener('click', () => {
-  message.textContent = "Thanks! We'll notify you when the site is live.";
-  btn.disabled = true;
-  btn.style.backgroundColor = '#6c757d'; // change button color to gray
-  btn.style.cursor = 'default';
-
-});
   
 } else {
   document.body.innerHTML = "<h1 style='color: red; text-align:center; margin-top: 50px;'>Access Denied</h1>";
